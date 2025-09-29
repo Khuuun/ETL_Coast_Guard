c7db9cd91276653804c56faf507506db4838cb1e
Khuuun <164432880+Khuuun@users.noreply.github.com>
2025-09-29 13:57:26 +0800

    Unstable v2

--- frontend/public/sidebar.js
+++
@@ -1,6 +0,0 @@
-const sideMenu = document.getElementById("side-menu");
-const sidebar = document.getElementById("sidebar-id");
-
-sideMenu.addEventListener("click", () => {
-  sidebar.classList.toggle("hidden");
-});
